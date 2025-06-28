238990735571b61f32bc8f94ce7fa32aa9e69adb
Anjanee Wijewardana <anjaneew@gmail.com>
2025-06-28 22:10:02 +0300

    input validation done. others still not finished

--- todoList.js
+++ todoList.js
@@ -4,18 +4,75 @@ const addBtn = document.getElementById("add-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const clearBtn = document.getElementById("clear-btn");
 const taskBox = document.getElementById("task-box");
+const taskList = document.getElementById("task-list");
 
+//just in case
+// localStorage.removeItem("tasksToDo");
+// localStorage.clear();
+
+let tasks = JSON.parse(localStorage.getItem("tasksToDo")) || [];
+
+//helper functions
+
+const cleanInput = (text) => {
+    const regex = /[^a-zA-Z\d\s-]/gi;
+    const cleanedText = text.trim().replace(regex, "");
+    console.log(cleanedText); //testing
+    return cleanedText;
+};
+
+// cleanInput("           Bj(ereO%$ 1f8df2er1g?? g   ");
+
+const display = () => {
+
+    // const liste = document.createElement("li");
+    // liste.textContent = task;
+    // taskList.appendChild(liste);
+
+    const currentList = JSON.parse(localStorage.getItem("tasksToDo"));
+
+    for(let one in currentList){
+        taskList.innerHTML = `<li>${one.task} -> ${one.date}</li>`;
+    }
+
+    taskInput.value = "";
+    dateInput.value = "";
+
+};
+
+
+
+//main functions
 function add() {
-    const task = taskInput.value.trim();
-    if (task === "") {
+    
+    let currentTask;
+    const taskTitle = cleanInput(taskInput.value);
+    const TaskDate = dateInput.value;
+    if (taskTitle === "") {
         alert("Please enter a task");
         return;
     }
-    const liste = document.createElement("li");
-    liste.textContent = task;
-    document.getElementById("taskList").appendChild(liste);
-    document.getElementById("taskInput").value = "";
+    else {
+        //saving values to program
+        currentTask = {task: taskTitle, date: TaskDate};
+        tasks.push(currentTask);
+        //saving to memory
+        localStorage.setItem("tasksToDo", JSON.stringify(tasks));
+        display();
+    }
+
 }
 
 
-taskInput.addEventListener("click", add);
+const deleteTask = () => {
+
+};
+
+const clear = () => {
+
+};
+
+//event Listeners for buttons
+addBtn.addEventListener("click", add);
+deleteBtn.addEventListener("click", deleteTask);
+clearBtn.addEventListener("click", clear);
